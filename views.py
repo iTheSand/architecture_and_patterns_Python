82da269f74d72632f92fce265d0b5d3452eef8b4
iTheSand <thesand2009@yandex.ru>
2021-12-01 12:30:00 +0300

    lesson_5_hw

---
+++ views.py
@@ -0,0 +1,141 @@
+from datetime import date
+
+from simba_framework.templator import render
+from patterns.сreational_patterns import Engine, Logger
+from patterns.structural_patterns import AppRoute, Debug
+
+site = Engine()
+logger = Logger('main')
+
+routes = {}
+
+
+# контроллер - главная страница
+@AppRoute(routes=routes, url='/')
+class Index:
+    @Debug(name='Index')
+    def __call__(self, request):
+        return '200 OK', render('index.html', data=date.today())
+
+
+@AppRoute(routes=routes, url='/contacts/')
+class Contacts:
+    @Debug(name='Contacts')
+    def __call__(self, request):
+        return '200 OK', render('contacts.html', data=date.today())
+
+
+@AppRoute(routes=routes, url='/courses/')
+class Courses:
+    @Debug(name='Courses')
+    def __call__(self, request):
+        return '200 OK', render('courses.html', objects_list=site.categories, data=date.today())
+
+
+# контроллер - список курсов
+@AppRoute(routes=routes, url='/courses-list/')
+class CoursesList:
+    @Debug(name='CoursesList')
+    def __call__(self, request):
+        logger.log('Список курсов')
+        try:
+            category = site.find_category_by_id(int(request['request_params']['id']))
+            return '200 OK', render('course_list.html', objects_list=category.courses, name=category.name,
+                                    id=category.id, data=date.today())
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
+
+
+# контроллер - создать курс
+@AppRoute(routes=routes, url='/create-course/')
+class CreateCourse:
+    category_id = -1
+
+    @Debug(name='CreateCourse')
+    def __call__(self, request):
+        logger.log('Создание курса')
+        if request['method'] == 'POST':
+            # метод пост
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category = None
+            if self.category_id != -1:
+                category = site.find_category_by_id(int(self.category_id))
+
+                course = site.create_course('record', name, category)
+                site.courses.append(course)
+
+            return '200 OK', render('course_list.html', objects_list=category.courses,
+                                    name=category.name, id=category.id, data=date.today())
+
+        else:
+            try:
+                self.category_id = int(request['request_params']['id'])
+                category = site.find_category_by_id(int(self.category_id))
+
+                return '200 OK', render('create_course.html', name=category.name, id=category.id, data=date.today())
+            except KeyError:
+                return '200 OK', 'No categories have been added yet'
+
+
+# контроллер - создать категорию
+@AppRoute(routes=routes, url='/create-category/')
+class CreateCategory:
+    @Debug(name='CreateCategory')
+    def __call__(self, request):
+        logger.log('Создание категории')
+        if request['method'] == 'POST':
+            # метод пост
+            # print(request)
+            data = request['data']
+
+            name = data['name']
+            name = site.decode_value(name)
+
+            category_id = data.get('category_id')
+
+            category = None
+            if category_id:
+                category = site.find_category_by_id(int(category_id))
+
+            new_category = site.create_category(name, category)
+
+            site.categories.append(new_category)
+
+            return '200 OK', render('courses.html', objects_list=site.categories, data=date.today())
+        else:
+            categories = site.categories
+            return '200 OK', render('create_category.html', categories=categories, data=date.today())
+
+
+# контроллер - список категорий
+@AppRoute(routes=routes, url='/category-list/')
+class CategoryList:
+    @Debug(name='CategoryList')
+    def __call__(self, request):
+        logger.log('Список категорий')
+        return '200 OK', render('category_list.html', objects_list=site.categories, data=date.today())
+
+
+# контроллер - копировать курс
+@AppRoute(routes=routes, url='/copy-course/')
+class CopyCourse:
+    @Debug(name='CopyCourse')
+    def __call__(self, request):
+        request_params = request['request_params']
+
+        try:
+            name = request_params['name']
+            old_course = site.get_course(name)
+            if old_course:
+                new_name = f'copy_{name}'
+                new_course = old_course.clone()
+                new_course.name = new_name
+                site.courses.append(new_course)
+
+            return '200 OK', render('course_list.html', objects_list=site.courses, data=date.today())
+        except KeyError:
+            return '200 OK', 'No courses have been added yet'
